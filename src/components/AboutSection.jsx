d6ddd20b6d08cbc87d2064a7d4e5fdff0c06744d
Raisul Islam Tonmoy <sodiumrdx@gmail.com>
2025-12-06 16:13:07 +0600

    add pdf

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -31,7 +31,7 @@ const AboutSection = () => {
               </a>
 
               <a
-                href="Tonmoy_CV.pdf"
+                href="Raisul Islam Tonmoy Mern Stack Developer.pdf"
                 download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/50 transition-colors duration-300 "
               >
